1102fccf24423c5d25fa3c317af6d2042aa16ad7
waltujo <henrialqui@gmail.com>
2024-12-06 12:13:31 -0300

    update for production

--- EnviarMsg/EnviarMsg/obj/Debug/net5.0/EnviarMsg.AssemblyInfo.cs
+++ EnviarMsg/EnviarMsg/obj/Debug/net5.0/EnviarMsg.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnviarMsg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f9e6cfd875232fa5f4dfca0fc1f37104eb63565")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff5cae66fe1b6173981df5c3914403d0b37303dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnviarMsg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnviarMsg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
